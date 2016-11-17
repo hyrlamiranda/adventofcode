9ee18ae62586d2ff51a85e7db1bab2abea29f2a1
hyrlamiranda <hyrlammiranda@gmail.com>
2016-11-17 23:44:49 +0100

    Solving day01

--- day01/day01.py
+++ day01/day01.py
@@ -3,7 +3,14 @@ Solve day 1 of Advent of Code.
 http://adventofcode.com/day/1
 """
 
-#left  = [0, 1, 4, 7, 8]
-#right = [2, 3, 5, 6, 9]
+instruction = {'(': 1, ')': -1 }
+#right = 3537
+#left = 3463
 
 def floor_frominstructions(instructions):
+    return sum([instruction.get(x, 0) for x in instructions])
+def position_of_basement(instructions):
+    if i in + 1:
+        return -1
+    else floor_frominstructions(instructions) == -1:
+        return i
